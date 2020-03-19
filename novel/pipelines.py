909f5d265d0274ff1aaad39d23c1e427102f1c69
kangkai1314 <1253393765@qq.com>
2020-03-19 17:24:22 +0800

    add db connecttion

--- novel/pipelines.py
+++ novel/pipelines.py
@@ -93,13 +93,60 @@ class TestPipeLine(object):
         yield item
 
 
-class MongoPipeLine(object):
+class MongoDbPipeline(object):
+    collections='task'
+
+    def __init__(self,url,db):
+        self.url=url
+        self.db=db
+
+    @classmethod
+    def from_crawler(cls,crawler):
+        return cls(
+            url=crawler.settings.get('MONGO_URI'),
+            db=crawler.settings.get('MONGO_DB')
+        )
+
+    def open_spider(self,spider):
+        self.client=pymongo.MongoClient(self.url)
+        self.mdb=self.client[self.db]
+
+    def close_spider(self,spider):
+        self.client.close()
+
+    def process_item(self,item,spider):
+        if not item['job']:
+            return item
+        data={
+            'job_name':item['job'].strip()
+        }
+        table=self.mdb[self.collections]
+        table.insert_one(data)
+        return item
+
+import pymysql
+class MysqlDbPipeline(object):
+
     def __init__(self):
-        host=settings['MONGODB_HOST']
-        port=settings['MONGODB_PORT']
-        dbname=settings['MONGODB_DB']
-        client=pymongo.MongoClient(host=host,port=port)
-        db=client[dbname]
+        dbparams = {
+            'host': '127.0.0.1',
+            'port': 3306,
+            'user': 'root',
+            'password': '19930304kk',
+            'database': 'myproject',
+            'charset': 'utf8'
+        }
+        self.conn=pymysql.connect(**dbparams)
+        self.cursor=self.conn.cursor()
+
+    def process_item(self,item,spider):
+        sql='''INSERT INTO liepin_job(job_name,publish_date) VALUES ( '%s', '%s')'''%(
+            item['job_name'],'2019-09-09'
+        )
+        print (sql)
+        self.cursor.execute(sql)
+        self.conn.commit()
+
 
 
 
